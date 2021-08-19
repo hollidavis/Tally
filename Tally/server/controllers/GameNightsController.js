a27a54a628d2efae61b8df462748b056d2163216
hollidavis <hollidavis19@gmail.com>
2021-08-19 16:41:25 -0600

    gameNight Controller/Service done

---
+++ Tally/server/controllers/GameNightsController.js
@@ -0,0 +1,41 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { gameNightsService } from '../services/GameNightsService'
+import BaseController from '../utils/BaseController'
+
+export class GameNightsController extends BaseController {
+  constructor() {
+    super('api/gamenights')
+    this.router
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .get('/:id', this.getById)
+      .post('', this.createGameNight)
+      .delete('/:id', this.removeGameNight)
+  }
+
+  async getById(req, res, next) {
+    try {
+      const gameNight = await gameNightsService.getById(req.params.id)
+      res.send(gameNight)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createGameNight(req, res, next) {
+    try {
+      const gameNight = await gameNightsService.createGameNight(req.body)
+      res.send(gameNight)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async removeGameNight(req, res, next) {
+    try {
+      await gameNightsService.removeGameNight(req.params.id)
+      res.send({ message: 'Successfully Deleted' })
+    } catch (error) {
+      next(error)
+    }
+  }
+}
--- Tally/server/services/GameNightsService.js
+++ Tally/server/services/GameNightsService.js
@@ -1,5 +1,26 @@
+import { dbContext } from '../db/DbContext'
+import { BadRequest } from '../utils/Errors'
 class GameNightsService {
+  async getById(id) {
+    const gameNight = await dbContext.GameNights.findById(id)
+    if (!gameNight) {
+      throw new BadRequest('Invalid Id')
+    }
+    return gameNight
+  }
 
+  async createGameNight(body) {
+    const gameNight = await dbContext.GameNights.create(body)
+    return await dbContext.GameNights.findById(gameNight.id)
+  }
+
+  async removeGameNight(id) {
+    const gameNight = await dbContext.GameNights.findByIdAndDelete(id)
+    if (!gameNight) {
+      throw new BadRequest('Invalid Id')
+    }
+    return gameNight
+  }
 }
 
 export const gameNightsService = new GameNightsService()
