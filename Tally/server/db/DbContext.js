c534069ddd4ab00ed312a2bd92267cd953030088
amanda-rice <aricericerice@gmail.com>
2021-08-19 16:26:00 -0600

    Account and Results Backend tested  and works

--- Tally/server/db/DbContext.js
+++ Tally/server/db/DbContext.js
@@ -4,6 +4,7 @@ import { GameSchema } from '../models/Game'
 import { GameNightSchema } from '../models/GameNight'
 import { HouseholdSchema } from '../models/Household'
 import { HouseholdProfileSchema } from '../models/HouseholdProfile'
+import { ResultSchema } from '../models/Result'
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
@@ -11,6 +12,8 @@ class DbContext {
   HouseholdProfiles = mongoose.model('HouseholdProfile', HouseholdProfileSchema);
   Games = mongoose.model('Game', GameSchema);
   GameNights = mongoose.model('GameNight', GameNightSchema);
+
+  Results = mongoose.model('Result', ResultSchema);
 }
 
 export const dbContext = new DbContext()
