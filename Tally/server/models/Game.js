543090c647e90473c89e2b291230d8904f8003e1
hollidavis <hollidavis19@gmail.com>
2021-08-19 15:18:11 -0600

    game model done

--- Tally/server/models/Game.js
+++ Tally/server/models/Game.js
@@ -4,6 +4,13 @@ const ObjectId = Schema.Types.ObjectId
 
 export const GameSchema = new Schema(
   {
+    name: { type: String, required: true },
+    maxPlayer: { type: Number, required: true },
+    minPlayer: { type: Number, required: true },
+    gameImg: { type: String, required: true },
+    playTime: { type: Number, required: true },
+    playerAge: { type: Number, required: true },
+    websiteLink: { type: String, required: true },
     householdId: { type: ObjectId, ref: 'Household', required: true },
     gameApiId: { type: String, required: true }
 
