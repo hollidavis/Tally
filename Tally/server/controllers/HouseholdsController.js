46722035c8131da7932d6aa4969a36e1329ddbb8
hollidavis <hollidavis19@gmail.com>
2021-08-20 14:23:09 -0600

    Household Service/Controller

--- Tally/server/controllers/HouseholdsController.js
+++ Tally/server/controllers/HouseholdsController.js
@@ -7,5 +7,66 @@ export class HouseholdsController extends BaseController {
     super('api/households')
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
+      .get('/:id', this.getById)
+      .get('/:id/games', this.getGamesByHouseholdId)
+      .get('/:id/gamenights', this.getGameNightByHouseholdId)
+      .get('/:id/results', this.getResultsByHouseholdId)
+      .put('/:id', this.editHousehold)
+      .post('', this.createHousehold)
+  }
+
+  async getById(req, res, next) {
+    try {
+      const household = await householdsService.getById(req.params.id)
+      res.send(household)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getGamesByHouseholdId(req, res, next) {
+    try {
+      const games = await householdsService.getGamesByHouseholdId(req.params.id)
+      res.send(games)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getGameNightByHouseholdId(req, res, next) {
+    try {
+      const gameNights = await householdsService.getGameNightsByHouseholdId(req.params.id)
+      res.send(gameNights)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getResultsByHouseholdId(req, res, next) {
+    try {
+      const results = await householdsService.getResultsByHouseholdId(req.params.id)
+      res.send(results)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editHousehold(req, res, next) {
+    try {
+      req.body.ownerAccountId = req.userInfo.id
+      const household = await householdsService.editHousehold(req.params.id, req.body)
+      res.send(household)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createHousehold(req, res, next) {
+    try {
+      const household = await householdsService.createHousehold(req.body)
+      res.send(household)
+    } catch (error) {
+      next(error)
+    }
   }
 }
--- Tally/server/services/HouseholdsService.js
+++ Tally/server/services/HouseholdsService.js
@@ -1,5 +1,55 @@
-class HouseholdsService {
+import { dbContext } from '../db/DbContext'
+import { BadRequest, Forbidden } from '../utils/Errors'
 
+class HouseholdsService {
+  async getById(id) {
+    const household = await dbContext.Households.findById(id)
+    if (!household) {
+      throw new BadRequest('Invalid Id')
+    }
+    return household
+  }
+
+  async getGamesByHouseholdId(id) {
+    const games = await dbContext.Games.find({ householdId: id })
+    if (!games) {
+      throw new BadRequest('Invalid Id')
+    }
+    return games
+  }
+
+  async getGameNightsByHouseholdId(id) {
+    const gameNights = await dbContext.GameNights.find({ householdId: id })
+    if (!gameNights) {
+      throw new BadRequest('Invalid Id')
+    }
+    return gameNights
+  }
+
+  async getResultsByHouseholdId(id) {
+    const results = await dbContext.Results.find({ householdId: id })
+    if (!results) {
+      throw new BadRequest('Invalid Id')
+    }
+    return results
+  }
+
+  async editHousehold(id, body) {
+    const household = await this.getById(id)
+    if (!household) {
+      throw new BadRequest('Invalid Id')
+    }
+    if (household.ownerAccountId.toString() !== body.ownerAccountId) {
+      throw new Forbidden('This is not your household')
+    }
+    const updatedHousehold = await dbContext.Households.findByIdAndUpdate(id, body, { new: true })
+    return updatedHousehold
+  }
+
+  async createHousehold(body) {
+    const household = await dbContext.Households.create(body)
+    return await dbContext.Households.findById(household.id)
+  }
 }
 
 export const householdsService = new HouseholdsService()
