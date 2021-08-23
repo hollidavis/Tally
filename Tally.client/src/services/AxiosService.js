70bd8fec3b0d53a0058e74573a6564c01a0d8c2a
hollidavis <hollidavis19@gmail.com>
2021-08-23 13:24:42 -0600

    renamed api to tallyApi

--- Tally.client/src/services/AxiosService.js
+++ Tally.client/src/services/AxiosService.js
@@ -1,6 +1,11 @@
 import Axios from 'axios'
 import { baseURL } from '../env'
-export const api = Axios.create({
+export const tallyApi = Axios.create({
+  baseURL,
+  timeout: 8000
+})
+
+export const gameApi = Axios.create({
   baseURL,
   timeout: 8000
 })
--- Tally.client/src/services/ProfilesService.js
+++ Tally.client/src/services/ProfilesService.js
@@ -1,10 +1,10 @@
 import { AppState } from '../AppState'
 import { logger } from '../utils/Logger'
-import { api } from './AxiosService'
+import { tallyApi } from './AxiosService'
 
 class ProfilesService {
   async getProfileById(id) {
-    const res = await api.get('account/' + id)
+    const res = await tallyApi.get('account/' + id)
     AppState.activeProfile = res.data
     logger.log('from service', AppState.activeProfile)
   }
