ba9f81ba7cdff150f246b92ff6e31b84c0bc213f
hollidavis <hollidavis19@gmail.com>
2021-08-24 10:35:51 -0600

    added remaining colors to sass variables

--- Tally.client/src/pages/AboutPage.vue
+++ Tally.client/src/pages/AboutPage.vue
@@ -1,7 +1,37 @@
 <template>
   <div class="container-fluid">
     <Navbar />
-    <h1>This is the about page</h1>
+    <div class="row justify-content-around">
+      <!-- Mission Statement -->
+      <div class="col-md-5 bg-white rounded p-2 mb-3">
+        <h1 class="text-center">
+          Mission Statement:
+        </h1>
+        <p class="blockquote m-0">
+          Tally exists to provide a space which fosters friendly competition by making hosting and scoring game nights a breeze.
+        </p>
+      </div>
+      <!-- Our Story -->
+      <div class="col-md-5 bg-white rounded p-2 mb-3">
+        <h1 class="text-center">
+          Our Story:
+        </h1>
+        <p class="blockquote m-0">
+          Tally was born at Boise CodeWorks, a software development bootcamp where the team met. Every day at lunch we would play board games together and compete for the high score. When it came time to begin working on a large project for our portfolios we all wanted to take the
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <!-- Who We Are -->
+      <div class="col-md-8 bg-white rounded p-2 mb-3">
+        <h1 class="text-center">
+          Mission Statement:
+        </h1>
+        <p class="blockquote m-0">
+          Tally exists to provide a space which fosters friendly competition by making hosting and scoring game nights a breeze.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
