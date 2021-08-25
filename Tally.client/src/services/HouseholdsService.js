55ffcffb80a0a24768cdc913bda1de5eb8aeefe2
amanda-rice <aricericerice@gmail.com>
2021-08-25 10:11:06 -0600

    computed on household service

--- Tally.client/src/services/HouseholdsService.js
+++ Tally.client/src/services/HouseholdsService.js
@@ -5,7 +5,7 @@ import { tallyApi } from './AxiosService'
 class HouseholdsService {
   async getHouseholdById(id) {
     const res = await tallyApi.get('account/' + id + '/households')
-    // AppState.activeProfile = res.data
+    AppState.myHousehold = res.data
   }
 }
 
