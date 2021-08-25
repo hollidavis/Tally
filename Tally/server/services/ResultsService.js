d4747639a8a1bf608465f53e5cae934bbb7c5029
hollidavis <hollidavis19@gmail.com>
2021-08-25 10:41:18 -0600

    results controller/service

--- Tally/server/services/ResultsService.js
+++ Tally/server/services/ResultsService.js
@@ -1,14 +1,21 @@
 import { dbContext } from '../db/DbContext'
 
 class ResultsService {
-  async getAllResults(query = {}) {
-    const results = await dbContext.Results.find(query).populate('profile', 'name picture').populate('game', 'name')
-    return results
-  }
-
-  async create(body) {
+  /** creates new result object using provided body
+   * @param {Object} body - includes details of results object to be created
+   * @returns new result object
+  */
+  async createResult(body) {
     const result = await dbContext.Results.create(body)
     return await dbContext.Results.find(result.id).populate('profile', 'name picture').populate('game', 'name')
+  }
+
+  /** Gets all result objects in data base. Not currently used but keeping for potential future use
+ * @returns array of all result objects in database
+*/
+  async getAllResults() {
+    const results = await dbContext.Results.find().populate('profile', 'name picture').populate('game', 'name')
+    return results
   }
 }
 
