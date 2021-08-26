0638598f8a7f51b6305a5a1504af4da44234a4eb
Veronica Blake/Noyce <vrex149@gmail.com>
2021-08-26 16:54:42 -0600

    end of day ✌🌛

--- Tally.client/src/components/GameCabinetCard.vue
+++ Tally.client/src/components/GameCabinetCard.vue
@@ -15,7 +15,7 @@
       </div>
     </div>
     <div class="row m-0 w-100 bg-white rowHeight">
-      <div v-for="game in games.games" :key="game.gameApiId">
+      <div v-for="game in games" :key="game.gameApiId">
         <!-- TODO style game cabinet item? Of we want it to be more than a name. Find out who is doing this -->
         <GameCabinetItem :game="game" />
       </div>
