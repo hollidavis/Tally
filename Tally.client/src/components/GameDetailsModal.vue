208848683721b6c56367e63dd34fad61216643ec
aaronpeet <aaronpeet@icloud.com>
2021-08-26 16:46:43 -0600

    game details modal

--- Tally.client/src/components/GameDetailsModal.vue
+++ Tally.client/src/components/GameDetailsModal.vue
@@ -11,22 +11,22 @@
       <div class="modal-content">
         <div class="modal-header">
           <h3 class="modal-title">
-            {{game.name}}
+            {{ game.name }}
           </h3>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-           <div class="p-3 bg-light rounded shadow my-3 d-flex align-items-center">
-    <img class="thumb-url"
-         :src="game.largeImg"
-         :alt="game.name"
-    >
-    <h3 class="ml-3 mr-auto m-0">
-      {{ game.description }}
-    </h3>
-  </div>
+          <div class="p-3 bg-light rounded shadow my-3 d-flex align-items-center">
+            <img class="thumb-url"
+                 :src="game.largeImg"
+                 :alt="game.name"
+            >
+            <h3 class="ml-3 mr-auto m-0">
+              {{ game.description }}
+            </h3>
+          </div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">
@@ -40,16 +40,14 @@
 
 <script>
 
-
 export default {
-    props:{
-        game:{
-            type: Object,
-            required: true
-        }
-    },
+  props: {
+    game: {
+      type: Object,
+      required: true
+    }
+  },
   setup() {
- 
     return {}
   },
   components: {}
