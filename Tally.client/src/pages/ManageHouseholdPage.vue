b9602082c7d3c92fb367cc213473e0fc03e39812
Veronica Blake/Noyce <vrex149@gmail.com>
2021-08-26 14:47:11 -0600

    before merging to main

--- Tally.client/src/pages/ManageHouseholdPage.vue
+++ Tally.client/src/pages/ManageHouseholdPage.vue
@@ -5,27 +5,8 @@
       <div class="col-md-12">
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-5 p-0">
-        <div class="row m-0 w-100 bg-dark-pink py-3">
-          <div class="col-md-10 p-0 text-center ">
-            <h1>
-              Household Game Cabinet
-            </h1>
-          </div>
-          <div id="Add Game Button"
-               class="col-md-2 p-0"
-          >
-            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#searchGameModal" title="Add Game to Cabinet">
-              <i class="fas fa-plus fa-xl"></i>
-            </button>
-          </div>
-        </div>
-        <div class="row m-0 w-100 bg-white rowHeight">
-          <!-- <div v-for="game in games" :key="game.gameApiId"></div> -->
-          <!-- TODO add run the for loop for profiles in the household, make/add profile card  -->
-        </div>
-      </div>
+    <div class="row m-0">
+      <GameCabinetCard :games="games" />
     </div>
     <SearchGameModal />
   </div>
