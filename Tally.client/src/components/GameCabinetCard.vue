e9555b2b2761a62915f12ffcf29dd4cd837d8c1d
Timothy Kimble <timble417@gmail.com>
2021-08-27 16:18:47 -0600

    removed game cabinet button

--- Tally.client/src/components/GameCabinetCard.vue
+++ Tally.client/src/components/GameCabinetCard.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="col-md-12 p-0">
     <div class="row m-0 w-100 bg-dark-pink py-3">
-      <div class="col-md-10 p-0 text-center ">
+      <div class="col-md-12 p-0 text-center ">
         <h1>
           Household Game Cabinet
         </h1>
-      </div>
-      <div id="Add Game Button"
-           class="col-md-2 p-0"
-      >
-        <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#searchGameModal" title="Add Game to Cabinet">
-          <i class="fas fa-plus fa-xl"></i>
-        </button>
       </div>
     </div>
     <div class="row m-0 w-100 bg-white rowHeight">
