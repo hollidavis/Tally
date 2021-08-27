1415c53897f8c51f8d2b6cc00b8d2f5ee457246d
Veronica Blake/Noyce <vrex149@gmail.com>
2021-08-27 14:43:35 -0600

    working on household member card

---
+++ Tally.client/src/components/HouseholdMembersCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row m-0 w-100 bg-dark-pink d-flex justify-content-center py-3">
+    <div class="col-md-10 p-0 text-center ">
+      <h2>
+        Members
+      </h2>
+    </div>
+  </div>
+  <div class="row m-0 w-100 bg-white rowHeight mb-3">
+    <div v-for="game in games" :key="game.gameApiId">
+      <HouseholdMemberItem />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HouseholdMembersCard',
+  setup() {
+    return {}
+  },
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
