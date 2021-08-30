ad3f614de67bbe4ddf0e289fa290e210bd516a46
hollidavis <hollidavis19@gmail.com>
2021-08-30 12:08:04 -0600

    fixed profile img alt

--- Tally.client/src/components/HouseholdMemberItem.vue
+++ Tally.client/src/components/HouseholdMemberItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex align-items-center bg-white rounded shadow my-2 py-2">
     <div class="p-0 d-flex px-3">
-      <img class="profile-img" :src="member.profile.picture" :alt="member.name">
+      <img class="profile-img" :src="member.profile.picture" :alt="member.profile.name">
     </div>
     <h5 class="m-0 w-50 mr-auto text-break">
       {{ member.profile.name }}
